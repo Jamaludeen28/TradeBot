8bba482ad3cd27ac3ec8d0cf4fe677d91698c2e1
Jamaludeenr <jamaljamaludeen36@gmail.com>
2024-07-18 15:36:51 +0530

    first push

---
+++ Node/Pair_4.js
@@ -0,0 +1,225 @@
+// file1.js
+const fs = require("fs");
+const express = require("express");
+const app = express();
+const BP = require("body-parser");
+app.use(BP.json());
+const axios = require("axios");
+const { DateTime } = require("luxon");
+const mysql = require("mysql");
+const cors = require("cors");
+
+const BigNumber = require("bignumber.js");
+const https = require("https");
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use(cors("*"));
+
+const polygon_pairs = fs.readFileSync("PolygonPairs.json", "utf8");
+
+function divideArray(array, chunkSize, index) {
+  var dividedArrays = [];
+  let len = Math.round(array.length / chunkSize);
+  let qwer = index * len;
+  //console.log(len, qwer);
+  for (let i = qwer; i < qwer + len; i++) {
+    dividedArrays.push(array[i]);
+  }
+  return dividedArrays;
+}
+
+async function GenerateAIpairs4(data) {
+  console.log("Calling 4");
+
+  try {
+    const amount = data.amount;
+    let network;
+    let pairs;
+    if (data.Network === "Polygon") {
+      pairs = JSON.parse(polygon_pairs);
+      network = 137;
+    } else if (data.Network === "Optimism") {
+      pairs = JSON.parse(optimism_pairs);
+      network = 10;
+    }
+
+    const filterValue = data.symbol;
+    const filteredPairs = pairs.filter((pair) => pair.from === filterValue);
+    let index = 3;
+    const dividedArrays = divideArray(filteredPairs, 5, index);
+    let newArray = [];
+    await Promise.all(
+      dividedArrays.map(async (pair, index) => {
+        const delayTime = 300;
+        await new Promise((resolve) => setTimeout(resolve, delayTime));
+
+        const openOceanData = await generateAIopenOcean(pair, amount, network);
+
+        let zeroXData = {
+          Dex: "ZeroX",
+          buyAmountZeroX: 0,
+          sellAmountZeroX: 0,
+        };
+        let oneInchData = {
+          Dex: "OneInch",
+          buyAmountOneInch: 0,
+          sellAmountOneInch: 0,
+        };
+        let paraSwapData = {
+          Dex: "Para Swap",
+          buyAmountParaSwap: 0,
+          sellAmountParaSwap: 0,
+        };
+
+        const buyResponse = Math.max(
+          openOceanData.buyAmountOpenOcean,
+          oneInchData.buyAmountOneInch,
+          zeroXData.buyAmountZeroX,
+          paraSwapData.buyAmountParaSwap
+        );
+        const sellresponse = Math.max(
+          openOceanData.sellAmountOpenOcean,
+          oneInchData.sellAmountOneInch,
+          zeroXData.sellAmountZeroX,
+          paraSwapData.sellAmountParaSwap
+        );
+
+        let bestBuyDex;
+        if (Number(buyResponse) === Number(openOceanData.buyAmountOpenOcean)) {
+          bestBuyDex = "Open Ocean";
+        } else if (
+          Number(buyResponse) === Number(oneInchData.buyAmountOneInch)
+        ) {
+          bestBuyDex = "One Inch";
+        } else if (Number(buyResponse) === Number(zeroXData.buyAmountZeroX)) {
+          bestBuyDex = "0x";
+        } else {
+          bestBuyDex = "Para Swap";
+        }
+
+        let bestSellDex;
+        if (
+          Number(sellresponse) === Number(openOceanData.sellAmountOpenOcean)
+        ) {
+          bestSellDex = "Open Ocean";
+        } else if (
+          Number(sellresponse) === Number(oneInchData.sellAmountOneInch)
+        ) {
+          bestSellDex = "One Inch";
+        } else if (Number(sellresponse) === Number(zeroXData.sellAmountZeroX)) {
+          bestSellDex = "0x";
+        } else {
+          bestSellDex = "Para Swap";
+        }
+
+        let inAmount = parseFloat(amount) + parseFloat(amount * (0.05 / 100));
+        let checkProfit = parseFloat(sellresponse) - parseFloat(inAmount);
+        const minus_plus =
+          checkProfit <= 0 ? -Math.abs(checkProfit) : Math.abs(checkProfit);
+        let profitPercent = (minus_plus / amount) * 100;
+
+        //console.log("Profit Percentage:", profitPercent.toFixed(2), "%");
+
+        const result = {
+          from_symbols: pair.from,
+          to_symbol: pair.to,
+          from_dex: bestBuyDex,
+          to_dex: bestSellDex,
+          buy_price: buyResponse,
+          sell_price: sellresponse.toFixed(4),
+          Profit_Amount: minus_plus,
+          Profit_Percentage: profitPercent.toFixed(3),
+          callData: openOceanData.callData,
+        };
+
+        if (profitPercent > 0) {
+          newArray.push(result);
+        }
+      })
+    );
+
+    return newArray;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function generateAIopenOcean(pair, Amount, network) {
+  const networkID = network;
+  const amount = Amount;
+
+  try {
+    const from = pair.from;
+    const to = pair.to;
+    const from_token_address = pair.from_contract;
+    const to_token_address = pair.to_contract;
+    const from_token_decimal = pair.from_decimal;
+    const to_token_decimal = pair.to_decimal;
+
+    //console.log("From:", from);
+    //console.log("To:", to);
+
+    const from_amount = new BigNumber(parseFloat(amount)).toFixed();
+
+    const openocean_config = {
+      headers: {
+        apikey: "Qdc1YAmzA6ZtzASUoXZQuULntrmP7UvI",
+      },
+    };
+
+    const openOceanBuyUrl = `https://open-api-pro.openocean.finance/v3/${networkID}/swap_quote?inTokenSymbol=${from}&inTokenAddress=${from_token_address}&outTokenSymbol=${to}&outTokenAddress=${to_token_address}&amount=${from_amount}&slippage=5&gasPrice=200&account=0x8d0A2e65a239c49C5Ca58F6ad38BffE450363b65`;
+    const openOceanBuyResponse = await axios.get(
+      openOceanBuyUrl,
+      openocean_config
+    );
+
+    const buyAmount =
+      openOceanBuyResponse.data.data.outAmount / 10 ** to_token_decimal;
+
+    if (openOceanBuyResponse && openOceanBuyResponse.data.data.outAmount) {
+      const openOceanSellUrl = `https://open-api-pro.openocean.finance/v3/${networkID}/swap_quote?inTokenSymbol=${to}&inTokenAddress=${to_token_address}&outTokenSymbol=${from}&outTokenAddress=${from_token_address}&amount=${buyAmount}&slippage=5&gasPrice=200&account=0x8d0A2e65a239c49C5Ca58F6ad38BffE450363b65`;
+      const openOceanSellResponse = await axios.get(
+        openOceanSellUrl,
+        openocean_config
+      );
+
+      if (openOceanSellResponse && openOceanSellResponse.data.data.outAmount) {
+        return {
+          Dex: "Open Ocean",
+          buyAmountOpenOcean: buyAmount,
+          sellAmountOpenOcean:
+            openOceanSellResponse.data.data.outAmount /
+            10 ** from_token_decimal,
+          callData: openOceanBuyResponse.data.data.data,
+        };
+      } else {
+        return {
+          Dex: "Open Ocean",
+          buyAmountOpenOcean: buyAmount,
+          sellAmountOpenOcean: 0,
+        };
+      }
+    } else {
+      return {
+        Dex: "Open Ocean",
+        buyAmountOpenOcean: 0,
+        sellAmountOpenOcean: 0,
+      };
+    }
+  } catch (error) {
+    //console.log(error);
+    return {
+      Dex: "Open Ocean",
+      buyAmountOpenOcean: 0,
+      sellAmountOpenOcean: 0,
+    };
+  }
+}
+
+module.exports = { GenerateAIpairs4 };
---
+++ Node/index.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const BP = require("body-parser");
+const axios = require("axios");
+const cors = require("cors");
+const fs = require("fs");
+const { GenerateAIpairs1 } = require("./Pair_1");
+const { GenerateAIpairs2 } = require("./Pair_2");
+const { GenerateAIpairs3 } = require("./Pair_3");
+const { GenerateAIpairs4 } = require("./Pair_4");
+const { GenerateAIpairs5 } = require("./Pair_5");
+
+const app = express();
+
+app.use(BP.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+const corsOptions = {
+  origin: ["http://localhost", "*"],
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+const fromTokens = JSON.parse(fs.readFileSync("FromPolygon.json", "utf8"));
+
+app.post("/fetchData", async (req, res) => {
+  const { token, amount } = req.body;
+  console.log(req.body)
+
+  try {
+    const selectedOption = fromTokens.find((t) => t.symbol === token);
+
+    if (!selectedOption) {
+      return res.status(400).send("Invalid token provided");
+    }
+
+    const pricedata = await axios.get(
+      `https://min-api.cryptocompare.com/data/price?api_key=eabd3a1fc2137ff50689349cfdf465d326902408eaa1cbaf2ec6fa744255a7e3&fsym=${token}&tsyms=USD`
+    );
+
+    const amountInUSD = amount / pricedata.data.USD;
+
+    const data = {
+      address: selectedOption.address,
+      symbol: selectedOption.symbol,
+      decimals: selectedOption.decimals,
+      amount: amountInUSD.toFixed(5),
+      Network: 'Polygon',
+      EnterAmount: amount,
+    };
+
+    const functionsToCall = [
+      GenerateAIpairs1,
+      GenerateAIpairs2,
+      GenerateAIpairs3,
+      GenerateAIpairs4,
+      GenerateAIpairs5,
+    ];
+
+    const responses = await Promise.all(
+      functionsToCall.map((func) => func(data))
+    );
+
+    const storePairs = responses.flat();
+    console.log("All responses:", storePairs.length);
+    res.send(storePairs);
+
+  } catch (error) {
+    console.log("Error while fetching: ", error);
+    res.status(500).send("Error while fetching data");
+  }
+});
+
+app.listen(3006, (err) => {
+  if (err) throw err;
+  console.log("Port running on 3006");
+});
